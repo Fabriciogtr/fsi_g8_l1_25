c9e5322f33b24e7f992d21b93a30849db97eba5d
fabricio techera <fabriciogtr96@gmail.com>
2025-03-29 18:12:28 -0300

    stuff

---
+++ decipher.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const run = async () => {
+  let wordCharAt = '';
+  const text = fs.readFileSync('./guia_practica_1.3.txt').toString();
+  for (line of text.split('\n')) {
+    for (char of line) {
+      if (char !== " ") {
+        if (char.charCodeAt(0) + 18 > 90)
+          wordCharAt += String.fromCharCode((char.charCodeAt(0) + 18 + 64) % 90)
+        else 
+          wordCharAt += String.fromCharCode((char.charCodeAt(0) + 18) % 90)
+      } else 
+        wordCharAt += char
+    }
+    wordCharAt += "\n"
+  }
+  console.log(
+    wordCharAt
+    .replaceAll('@', ".")
+    .replaceAll(':', "\"")
+    .replaceAll(';', "\"")
+    .replaceAll('>', ",")
+  )
+}
+run()
+// 18
