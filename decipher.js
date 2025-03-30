a21081a173ff02de4be5327673321e6e510877a6
ale8947 <alemu_sm@hotmail.com>
2025-03-29 23:06:11 -0300

    Texto completamente descifrado

--- decipher.js
+++ decipher.js
@@ -2,24 +2,32 @@ const fs = require('fs');
 const run = () => {
   let wordCharAt = '';
   const text = fs.readFileSync('./guia_practica_1.3.txt').toString();
+  let i = 0
   for (line of text.split('\n')) {
-    for (char of line) {
+    i ++ 
+    if (i === 6 || i === 7)
+      console.log("linea: ", line)
+    
+    for (char of line) { 
       if (char !== " ") {
-        if (char.charCodeAt(0) + 18 > 90)
-          wordCharAt += String.fromCharCode((char.charCodeAt(0) + 18 + 64) % 90)
-        else 
-          wordCharAt += String.fromCharCode((char.charCodeAt(0) + 18) % 90)
-      } else 
+          if (char.charCodeAt(0) + 18 > 90){ // se pasa de arriba --> aplico modulo
+            wordCharAt += String.fromCharCode((char.charCodeAt(0) + 18 + 64) % 90)
+          }
+          else if (char.charCodeAt(0) > 64) { // está dentro del conjunto [65, .., 90] 
+            wordCharAt += String.fromCharCode((char.charCodeAt(0) + 18) % 90)
+          } else
+            wordCharAt += char
+      } else  
         wordCharAt += char
     }
     wordCharAt += "\n"
   }
   console.log(
     wordCharAt
-    .replaceAll('@', ".")
-    .replaceAll(':', "\"")
-    .replaceAll(';', "\"")
-    .replaceAll('>', ",")
+    // .replaceAll('@', ".")
+    // .replaceAll(':', "\"")
+    // .replaceAll(';', "\"")
+    // .replaceAll('>', ",")
   )
 }
 run()
