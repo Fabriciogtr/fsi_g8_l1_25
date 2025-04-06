5c1372da8c02cce642747f032e062257719ef171
fabricio techera <fabriciogtr96@gmail.com>
2025-04-06 14:14:18 -0300

    Practica 2

--- decipher.js
+++ decipher.js
@@ -4,26 +4,16 @@ const run = () => {
   const text = fs.readFileSync('./guia_practica_1.3.txt').toString();
   for (line of text.split('\n')) {
     for (char of line) { 
-      if (char !== " ") {
-          if (char.charCodeAt(0) + 18 > 90){ // se pasa de arriba --> aplico modulo
-            wordCharAt += String.fromCharCode((char.charCodeAt(0) + 18 + 64) % 90)
-          }
-          else if (char.charCodeAt(0) > 64) { // está dentro del conjunto [65, .., 90] 
-            wordCharAt += String.fromCharCode((char.charCodeAt(0) + 18) % 90)
-          } else
-            wordCharAt += char
-      } else  
+      if (char.charCodeAt(0) + 18 > 90){ // se pasa de arriba --> aplico modulo
+        wordCharAt += String.fromCharCode((char.charCodeAt(0) + 18 + 64) % 90)
+      }
+      else if (char.charCodeAt(0) > 64) { // está dentro del conjunto [65, .., 90] 
+        wordCharAt += String.fromCharCode((char.charCodeAt(0) + 18) % 90)
+      } else
         wordCharAt += char
     }
     wordCharAt += "\n"
   }
-  console.log(
-    wordCharAt
-    // .replaceAll('@', ".")
-    // .replaceAll(':', "\"")
-    // .replaceAll(';', "\"")
-    // .replaceAll('>', ",")
-  )
+  console.log(wordCharAt)
 }
 run()
-// 18
